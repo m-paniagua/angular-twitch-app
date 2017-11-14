cdfd113d88058da4d049d86c545c7dc8ccdb215f
Manuel Paniagua <manuel.paniagua84@gmail.com>
2017-11-14 00:13:21 -0800

    Allow list to be filtered

--- js/controllers/twitchController.js
+++ js/controllers/twitchController.js
@@ -1,18 +1,21 @@
 myApp.controller('TwitchController', ['$scope', '$q', 'TwitchFactory', function($scope, $q, TwitchFactory) {
     var userNames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "streamerhouse"]
-    $scope.channels;
-    $scope.streams;
+    $scope.channels = [];
+    $scope.streams = [];
 
     TwitchFactory.getChannels().then(
-        function(data) {
-            $scope.channels = data;
-            // console.log(data);
+        function(channels) {
+            for (let index = 0; index < channels.length; index++) {
+                $scope.channels.push(channels[index].data);
+            }
         }
     )
 
     TwitchFactory.getStreams().then(
-        function(data) {
-            $scope.streams = data;
+        function(streams) {
+            for (let index = 0; index < streams.length; index++) {
+                $scope.streams.push(streams[index].data);
+            }
         }
     )
 }])
